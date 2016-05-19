c434438a22e9e9b5836d41622158f9571c2d123c
gonghxin <gonghxin@163.com>
2016-05-19 18:11:14 +0800

    'dongtai'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+)
